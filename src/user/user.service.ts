cde74c1c6bb2d340e07ca1bccffae6fd4c727f1b
israelwerther <israelwerther48@outlook.com>
2023-05-22 11:09:09 -0300

    Aula 11 [NestJS] - Inclusão de validação na requisição POST do Nest.js - Done

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -7,26 +7,25 @@ import { Repository } from 'typeorm';
 
 @Injectable()
 export class UserService {
-    constructor(
-        @InjectRepository(UserEntity)
-        private readonly userRepository: Repository<UserEntity>,
-    ) {}
-    
+  constructor(
+    @InjectRepository(UserEntity)
+    private readonly userRepository: Repository<UserEntity>,
+  ) {}
 
-    async createUser(createUserDto: CreateUserDto): Promise<UserEntity> {
-        const saltOrRounds = 10;
-        
-        const passwordHashed = await hash(createUserDto.password, saltOrRounds);
+  async createUser(createUserDto: CreateUserDto): Promise<UserEntity> {
+    const saltOrRounds = 10;
 
-        return this.userRepository.save({
-            ...createUserDto,
-            typeUser: 1,
-            password: passwordHashed,
-        });
-    }
+    const passwordHashed = await hash(createUserDto.password, saltOrRounds);
 
-    async getAllUser(): Promise<UserEntity[]> {
-        return this.userRepository.find();
-    }
+    return this.userRepository.save({
+      ...createUserDto,
+      typeUser: 1,
+      password: passwordHashed,
+    });
+  }
 
+  async getAllUser(): Promise<UserEntity[]> {
+    console.log('Entrou no service');
+    return this.userRepository.find();
+  }
 }
