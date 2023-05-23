b88ea246e41707d8ae188710320ccf06ea88518f
israelwerther <israelwerther48@outlook.com>
2023-05-23 08:23:39 -0300

    Aula 12 [NestJS] - Criação do módulo de endereço - Done

--- src/address/entities/address.entity.ts
+++ src/address/entities/address.entity.ts
@@ -1,28 +1,33 @@
-import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn } from "typeorm";
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
 
 @Entity({ name: 'address' })
 export class AddressEntity {
-    @PrimaryGeneratedColumn('rowid')
-    id: number;
+  @PrimaryGeneratedColumn('rowid')
+  id: number;
 
-    @Column({ name: 'user_id', nullable: false })
-    userId: number;
+  @Column({ name: 'user_id', nullable: false })
+  userId: number;
 
-    @Column({ name: 'complement', nullable: true })
-    complement: string;
+  @Column({ name: 'complement', nullable: true })
+  complement: string;
 
-    @Column({ name: 'number', nullable: false })
-    numberAddress: number;
+  @Column({ name: 'number', nullable: false })
+  numberAddress: number;
 
-    @Column({ name: 'cep', nullable: false })
-    cep: string;
+  @Column({ name: 'cep', nullable: false })
+  cep: string;
 
-    @Column({ name: 'city_id', nullable: false })
-    cityId: number;
+  @Column({ name: 'city_id', nullable: false })
+  cityId: number;
 
-    @CreateDateColumn({ name: 'created_at' })
-    createdAt: Date;
+  @CreateDateColumn({ name: 'created_at' })
+  createdAt: Date;
 
-    @CreateDateColumn({ name: 'updated_at' })
-    updatedAt: Date;
+  @CreateDateColumn({ name: 'updated_at' })
+  updatedAt: Date;
 }
