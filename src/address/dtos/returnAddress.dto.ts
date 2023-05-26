1fafe74a44e924450f4dd91d71604b7ab7344fdd
israelwerther <israelwerther48@outlook.com>
2023-05-26 15:31:32 -0300

    Aula 15 [NestJS] - Criação de DTO's de estados e cidades - Done

--- src/address/dtos/returnAddress.dto.ts
+++ src/address/dtos/returnAddress.dto.ts
@@ -1,13 +1,16 @@
+import { ReturnCityDto } from 'src/city/dtos/returnCity.dto';
 import { AddressEntity } from '../entities/address.entity';
 
 export class ReturnAddressDto {
   complement: string;
   numberAddress: number;
   cep: string;
+  city?: ReturnCityDto;
 
   constructor(addess: AddressEntity) {
     this.complement = addess.complement;
     this.numberAddress = addess.numberAddress;
     this.cep = addess.cep;
+    this.city = addess.city ? new ReturnCityDto(addess.city) : undefined;
   }
 }
