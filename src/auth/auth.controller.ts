7db3c68033222f7810463f8653ab2f5804aed4d2
israelwerther <israelwerther48@outlook.com>
2023-05-30 10:28:53 -0300

    Aula 17 [NestJs] - Inclusão do token JWT na resposta do login - Done

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -6,8 +6,8 @@ import {
   ValidationPipe,
 } from '@nestjs/common';
 import { LoginDto } from './dtos/login.dto';
-import { ReturnUserDto } from 'src/user/dtos/returnUser.dto';
 import { AuthService } from './auth.service';
+import { ReturnLogin } from './dtos/returnLogin.dto';
 // Comments to be deleted
 // This controller was created just because is not using injectebles yet
 @Controller('auth')
@@ -16,7 +16,7 @@ export class AuthController {
 
   @UsePipes(ValidationPipe)
   @Post()
-  async login(@Body() loginDto: LoginDto): Promise<ReturnUserDto> {
-    return new ReturnUserDto(await this.authService.login(loginDto));
+  async login(@Body() loginDto: LoginDto): Promise<ReturnLogin> {
+    return this.authService.login(loginDto);
   }
 }
